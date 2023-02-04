8fba413e7e29fa6b7fa18e63fd2a3af4a9ff74ca
HenriqueAD (OSistemático) <56042159+henriquead7@users.noreply.github.com>
2023-02-04 17:03:47 -0300

    Add files via upload
    Menu Principal

---
+++ mangaq_menu.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+# autor: HenriqueAD <www.osistematico.com.br> <youtube.com/OSistematico>
+# descrição: Menu via utilitário Zenity, para acessar diversas funções em vários scripts do MangaQ.
+# version: 1.0
+# licença: MIT License
+
+# Menu principal do MangaQ
+
+#DIRETÓRIO DOS DEMAIS SCRIPTS, QUE DEVE ESTAR NO MESMO DIRETÓRIO DESTE SCRIPT 
+CAMINHO_SCRIPTS="$HOME/MangaQ/mangaq_scripts"
+
+#EXIBE MENSAGEM INICIAL
+zenity --info --text="Bem-vindo ao MangaQ, o script de compactação de pastas em CBZ"
+
+titulo="Imagens em CBZ com MangaQ"
+prompt="                                                          Escolha uma opção"
+options=(
+          "[0] ---> Sobre MangaQ"
+          "[1] ---> Criar único arquivo CBZ"
+          "[2] ---> Criar lote de arquivos CBZ"
+          "[3] ---> Sair"
+        )
+while opt=$(zenity --title="$titulo" --text="$prompt" --list --width=500 --height=410  \
+                   --column="Opções" --hide-header "${options[@]}"); do
+    
+    case "$opt" in
+
+    #SOBRE/FAQ
+    "${options[0]}" ) source $CAMINHO_SCRIPTS/mangaq_faq.sh 
+     ;;
+     
+    #CRIAR ÚNICO ARQUIVO CBZ
+    "${options[1]}" ) source $CAMINHO_SCRIPTS/mangaq_unico.sh 
+     ;;
+     
+    #CRIAR LOTE DE ARQUIVOS CBZ
+    "${options[2]}" ) cd $HOME
+                      source $CAMINHO_SCRIPTS/mangaq_lote.sh
+     ;;
+
+    #CRIAR LOTE DE ARQUIVOS CBZ
+    "${options[3]}" ) exit
+     ;;
+
+    *) zenity --error --text="Opção inválida!" --width=150 --height=100;;
+    esac
+
+done
+
+exit
+
