52cdcea48bd1c97e37122d788a2e8d1d06f3ff62
HenriqueAD (OSistemático) <56042159+henriquead7@users.noreply.github.com>
2023-02-04 17:07:22 -0300

    Add files via upload
    Scripts para o funcionamento do MangaQ

---
+++ mangaq_scripts/mangaq_faq.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# autor: HenriqueAD <www.osistematico.com.br> <youtube.com/OSistematico>
+# descrição: Pequeno FAQ via utilitário Zenity do MangaQ.
+# version: 1.0
+# licença: MIT License
+
+#EXIBE MENSAGEM FAQ
+zenity --info --title="SOBRE" --text="MangaQ é um utilitário que compacta suas imagens de mangás e HQ's no formato digital de leitura CBZ, em duas opções:
+
+'Criar único arquivo CBZ' --> que cria um arquivo CBZ de uma pasta sua com imagens;
+
+'Criar lote de arquivos CBZ' --> que cria vários arquivos CBZ, com base em uma pasta sua com várias pastas de imagens;
+
+Os arquivos originais ainda permanecem em seu sistema, é necessário a exclusão manual das pastas com as imagens. Este comportamento é esperado, para evitar eventuais erros num possível arquivo CBZ corrompido.
+
+O MangaQ é um utilitário licenciado sob a licença MIT. 
+
+Author: HenriqueAD 
+Site: www.osistematico.com.br 
+Youtube: youtube.com/OSistematico
+
+Não há garantias alguma com este software, utilize por sua conta e risco."
+
---
+++ mangaq_scripts/mangaq_lote.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+# autor: HenriqueAD <www.osistematico.com.br> <youtube.com/OSistematico>
+# descrição: Pequeno utilitário Zenity do MangaQ, que seleciona uma pasta contendo outras pastas com imagens e de forma recursiva compacta em vários arquivos em CBZ.
+# version: 1.0
+# licença: MIT License
+
+#MENSAGEM INICIAL
+zenity --info --text="MangaQ irá compactar suas pastas com imagens em vários arquivos CBZ"
+
+#SELECIONA A PASTA QUE SERÁ COMPACTADA EM CBZ
+pasta=$(zenity --file-selection --directory --title="Selecione a pasta com as imagens")
+
+#VERIFICA SE A PASTA FOI SELECIONADA
+if [ -z "$pasta" ]; then
+  zenity --error --text="Pasta não selecionada. Encerrando MangaQ..."
+  exit 1
+fi
+
+#CONTA A QUANTIDADE DE ARQUIVOS NA PASTA E QUANTAS PASTAS
+total_arquivos=$(find "$pasta" -type f | wc -l)
+total_pastas=$(ls -l $diretorio | grep ^d | wc -l)
+total_pastas=$((total_pastas + 1))
+arquivo_atual=0
+
+#SELECIONA O DESTINO FINAL DOS ARQUIVOS CBZ
+destino=$(zenity --file-selection --directory --title="Selecione o local de destino dos arquivos CBZ")
+
+#VERIFICA SE O DESTINO FOI SELECIONADO
+if [ -z "$destino" ]; then
+  zenity --error --text="Destino não selecionado. Encerrando MangaQ..."
+  exit 1
+fi
+
+#COMPACTA DE FORMA RECURSIVA TODAS AS PASTAS DENTRO DA PASTA SELECIONADA
+zenity --progress \
+  --title="Compactando arquivos" \
+  --text="Compactando arquivos para formato CBZ..." \
+  --percentage=0 \
+  --auto-close \
+  --pulsate \
+  --no-cancel &
+
+for arquivo in "$pasta"/*; do
+  if [ -d "$arquivo" ]; then
+    arquivo_atual=$((arquivo_atual + 1))
+    nome_arquivo=$(basename "$arquivo")
+    cd "$destino" || exit
+    zip -r "$nome_arquivo".cbz "$arquivo"
+    percentage=$(( 100 * arquivo_atual / total_arquivos ))
+    echo "$percentage"
+    echo "# Compactando '$nome_arquivo' --> $arquivo_atual de $total_pastas"
+  fi
+done | zenity --progress \
+  --title="Compactando arquivos" \
+  --text="Compactando arquivos para formato CBZ..." \
+  --percentage=0 \
+  --auto-close \
+  --pulsate \
+  --no-cancel
+
+#MENSAGEM DE SUCESSO
+zenity --info --text "Arquivos CBZ salvos em ''$destino/$nome_cbz''"
+
